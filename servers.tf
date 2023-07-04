d7315fe3179eafc3097bf6c4e49bcb826586180b
sreesai169 <sreesai169@gmail.com>
2023-07-04 07:36:37 +0530

    Committing terraform changes for creating instances

--- servers.tf
+++ servers.tf
@@ -1,62 +1,66 @@
+data "aws_ami""centos"{
+  owners = [973714476881]
+  most_recent = true
+  name_regex = "Centos-8-Devops-Practice"
+}
+
+
 resource "aws_instance" "frontend" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "frontend" }
 }
-output "frontend" {
-  value = aws_instance.frontend.public_ip
-}
 resource "aws_instance" "mongodb" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "mongodb" }
 }
 resource "aws_instance" "catalogue" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "catalogue" }
 }
 resource "aws_instance" "redis" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "redis" }
 }
 resource "aws_instance" "user" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "user" }
 }
 resource "aws_instance" "cart" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "cart" }
 }
 resource "aws_instance" "mysql" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "mysql" }
 }
 resource "aws_instance" "shipping" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "shipping" }
 }
 resource "aws_instance" "rabbitmq" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "rabbitmq" }
 }
 resource "aws_instance" "payment" {
-  ami              =   "ami-03265a0778a880afb"
+  ami              =   data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = { Name = "payment" }
