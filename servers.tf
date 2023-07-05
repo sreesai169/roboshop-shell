2c35bb708b1ec72419c157444209a3d9bfd85471
sreesai169 <sreesai169@gmail.com>
2023-07-05 17:41:00 +0530

    Committing terraform changes for creating instances

--- servers.tf
+++ servers.tf
@@ -4,7 +4,24 @@ resource "aws_instance""instance"{
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
   tags = { Name = each.value["name"]}
+  provisioner "remote-exec" {
+    connection {
+      type="ssh"
+      user="root"
+      password = "DevOps321"
+      host = self.private_ip
+    }
+
+    inline=[
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/sreesai169/roboshop-shell.git",
+      "cd roboshop-shell",
+      "bash ${each.value["name"]}.sh"
+    ]
+  }
+
 }
+
 resource "aws_route53_record""records"{
   for_each = var.component
   name    = "${each.value["name"]}-dev.devops16.online"
