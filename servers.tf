227636ce5f24982bb1b5a0c9ca050ba02484c33b
sreesai169 <sreesai169@gmail.com>
2023-07-05 11:15:21 +0530

    Committing terraform changes for creating instances

--- servers.tf
+++ servers.tf
@@ -10,14 +10,55 @@ data "aws_security_group" "allow-all"{
   name ="allow-all"
 }
 variable "component" {
-  default = ["frontend","mongodb","catalogue"]
+ default = {
+   frontend = {
+     name = "frontend"
+     instance_type = "t3.small"
+   }
+   mongodb={
+     name = "mongodb"
+     instance_type = "t3.small"
+   }
+   catalogue = {
+     name = "catalogue"
+     instance_type = "t3.micro"
+   }
+   redis = {
+     name = "redis"
+     instance_type = "t3.small"
+   }
+   user = {
+     name = "user"
+     instance_type = "t3.micro"
+   }
+   cart = {
+     name = "cart"
+     instance_type = "t3.micro"
+   }
+   mysql = {
+     name = "mysql"
+     instance_type = "t3.small"
+   }
+   shipping = {
+     name = "shipping"
+     instance_type = "t3.medium"
+   }
+   rabbitmq = {
+     name = "rabbitmq"
+     instance_type = "t3.small"
+   }
+   payment = {
+     name = "payment"
+     instance_type = "t3.small"
+   }
+ }
 }
 resource "aws_instance""instance"{
-  count = length(var.component)
+  for_each = var.component
   ami              =   data.aws_ami.centos.image_id
-  instance_type = var.instance_type
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-  tags = { Name = var.component[count.index] }
+  tags = { Name = each.value["name"]}
 }
 #resource "aws_instance" "frontend" {
 #
