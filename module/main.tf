73edc8b00f1c323275e3db6c170f73e46ed7d3b2
sreesai169 <sreesai169@gmail.com>
2023-07-06 15:13:38 +0530

    Committing terraform changes for creating instances

--- module/main.tf
+++ module/main.tf
@@ -1,8 +1,7 @@
 
 resource "aws_instance" "instance"{
-  for_each = var.component
   ami              =   data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
+  instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
   tags = { Name = var.component_name}
 }
