31d22ef600c6420be21a9b2afcdb275eaa54aee8
sreesai169 <sreesai169@gmail.com>
2023-07-06 07:52:58 +0530

    Committing terraform changes for creating instances

--- servers.tf
+++ servers.tf
@@ -20,7 +20,7 @@ resource "null_resource""provisioner"{
       "rm -rf roboshop-shell",
       "git clone https://github.com/sreesai169/roboshop-shell.git",
       "cd roboshop-shell",
-      "bash ${each.value["name"]}.sh ${each.value["password"]}"
+      "bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null")}"
     ]
   }
 }
