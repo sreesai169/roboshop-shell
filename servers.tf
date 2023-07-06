64dfb730051cee9e65bced24489ac4f98d9863bf
sreesai169 <sreesai169@gmail.com>
2023-07-06 08:04:27 +0530

    Committing terraform changes for creating instances

--- servers.tf
+++ servers.tf
@@ -5,7 +5,7 @@ resource "aws_instance""instance"{
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
   tags = { Name = each.value["name"]}
 }
-resource "null_resource""provisioner"{
+resource "null_resource" "provisioner"{
   depends_on = [aws_instance.instance,aws_route53_record.records]
   for_each = var.component
   provisioner "remote-exec" {
