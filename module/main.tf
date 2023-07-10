57b1886af3622cf41b35eb011062cce697578fa5
sreesai169 <sreesai169@gmail.com>
2023-07-10 10:36:33 +0530

    Committing terraform changes for creating instances

--- module/main.tf
+++ module/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "instance"{
   ami              =   data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = local.name
   }
@@ -51,6 +52,12 @@ resource "aws_iam_role" "role" {
     tag-key = "${var.component_name}-${var.env}-role"
   }
 }
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component_name}-${var.env}-role"
+  role = aws_iam_role.role.name
+}
+
 resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
   role = aws_iam_role.role.id
