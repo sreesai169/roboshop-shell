74b93b2a4f1f1ad230bba2d3ee609c2218fdbad1
sreesai169 <sreesai169@gmail.com>
2023-07-12 19:26:41 +0530

    Committing terraform changes for creating instances

--- module/locals.tf
+++ module/locals.tf
@@ -10,6 +10,14 @@ locals {
   app_commands=[
     "labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/sreesai169/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
-
   ]
+  db_tags= {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags= {
+    Name = "${var.component_name}-${var.env}"
+    Monitor = "true"
+    component = var.component_name
+    env = var.env
+  }
 }
