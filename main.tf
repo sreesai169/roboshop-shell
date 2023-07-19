a7e54602abc3aac9127e54460784cc6c895383fc
sreesai169 <sreesai169@gmail.com>
2023-07-19 16:17:50 +0530

    Committing terraform changes for creating instances

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ module "app" {
   min_size = each.value["min_size"]
   env = var.env
   bastion_cidr = var.bastion_cidr
+  tags = local.tags
   instance_type = each.value["instance_type"]
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
   vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
